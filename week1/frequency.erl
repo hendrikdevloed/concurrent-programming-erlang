c8f1ab979a5383436c519a026ee9287bba0d9e4f
Hendrik De Vloed <Hendrik De Vloed>
2017-04-06 01:26:02 +0200

    Soluton

--- week1/frequency.erl
+++ week1/frequency.erl
@@ -23,10 +23,13 @@ init() ->
 
 % Hard Coded
 get_frequencies() -> [10,11,12,13,14,15].
+% Set to true to simulate an overloaded server
+debug_simulate_overload() -> false.
 
 %% The Main Loop
 
 loop(Frequencies) ->
+  case debug_simulate_overload() of true -> timer:sleep(4000); false -> ok end,
   receive
     {request, Pid, allocate} ->
       {NewFrequencies, Reply} = allocate(Frequencies, Pid),
@@ -42,28 +45,40 @@ loop(Frequencies) ->
 
 %% Functional interface
 
-allocate() -> 
+allocate() ->
+    clear(), % Ensure no stale replies exist
     frequency ! {request, self(), allocate},
     receive 
 	    {reply, Reply} -> Reply
+    after 2000 -> {error, timeout}
     end.
 
 deallocate(Freq) -> 
+    clear(), % Ensure no stale replies exist
     frequency ! {request, self(), {deallocate, Freq}},
     receive 
 	    {reply, Reply} -> Reply
+    after 2000 -> {error, timeout}
     end.
 
 stop() -> 
+    clear(), % Ensure no stale replies exist
     frequency ! {request, self(), stop},
     receive 
 	    {reply, Reply} -> Reply
+    after 2000 -> {error, timeout}
     end.
 
 % Flushing the mailbox
 clear() ->
   receive
-    _Msg -> clear()
+    Msg ->
+      % Show what we're discarding if debugging
+      case debug_simulate_overload() of
+        true -> io:format("(Discarded old reply ~w)~n", [Msg]);
+        false -> ok
+      end,
+      clear()
   after 0 -> ok
   end.
 
@@ -73,15 +88,20 @@ clear() ->
 allocate({[], Allocated}, _Pid) ->
   {{[], Allocated}, {error, no_frequency}};
 allocate({[Freq|Free], Allocated}, Pid) ->
+  % Check if the requesting Pid has already allocated a frequency
   case lists:keyfind(Pid, 2, Allocated) of
-    false -> {{Free, [{Freq, Pid}|Allocated]}, {ok, Freq}};
-    _ -> {{[], Allocated}, {error, quota_exceeded}}
+    false -> % Allocate a new frequency
+      {{Free, [{Freq, Pid}|Allocated]}, {ok, Freq}};
+    _Found -> % Refuse request: quotum of 1 frequency per pid
+      {{[Freq|Free], Allocated}, {error, quota_exceeded}}
   end.
 
 deallocate({Free, Allocated}, Freq, Pid) ->
+  % Check if the requesting Pid owns the frequency it is deallocating
   case lists:member({Freq, Pid}, Allocated) of
-    false -> {{Free, Allocated}, permission_denied};
-    true ->
+    false -> % Refuse deallocateion
+      {{Free, Allocated}, {error, permission_denied}};
+    true -> % Deallocate
       NewAllocated=lists:keydelete(Freq, 1, Allocated),
       {{[Freq|Free],  NewAllocated}, ok}
   end.
